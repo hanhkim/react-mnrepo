bb090dbb586d754676fb1b110d9ba5a7bf5280fa
hali <hanh.ltk@carehealth.io>
2024-02-27 22:42:08 +0700

    init repo

--- apps/react-monorepo/src/app/app.tsx
+++ apps/react-monorepo/src/app/app.tsx
@@ -1,13 +1,20 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.css';
+import { Route, Routes } from 'react-router-dom';
+import { Products } from '@react-monorepo/products';
 
-import NxWelcome from './nx-welcome';
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function ProductList() {
+  return <div>ProductList</div>;
+}
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="react-monorepo" />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/products" element={<Products />}></Route>
+    </Routes>
   );
 }
 
